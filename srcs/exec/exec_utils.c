179301fc48394cce4392c826ceb48eca8eef0a99
Ajvar <cueille@protonmail.com>
2021-07-04 20:35:36 +0200

    gnl must be normed

--- srcs/exec/exec_utils.c
+++ srcs/exec/exec_utils.c
@@ -6,7 +6,7 @@
 /*   By: jcueille <jcueille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/11 20:15:26 by jcueille          #+#    #+#             */
-/*   Updated: 2021/06/30 21:48:25 by jcueille         ###   ########.fr       */
+/*   Updated: 2021/07/04 20:10:16 by jcueille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,6 @@ int	is_builtin(t_list *cmds)
 	if (r < 0)
 		ft_putstr_fd("Error executing builtin.\n", 2);
 	free_list(tmp);
-	printf("r is %d\n", r);
 	return (r);
 }
 
