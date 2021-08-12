07aaf255a6eae70a4548ab39b5f064d660860c42
Arthur-TRT <atrouill@student.42.fr>
2021-08-12 11:35:47 +0200

    Improve args parsing in ft_echo.c

--- srcs/builtins/ft_echo.c
+++ srcs/builtins/ft_echo.c
@@ -3,14 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   ft_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jcueille <jcueille@student.42.fr>          +#+  +:+       +#+        */
+/*   By: atrouill <atrouill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/31 14:38:23 by jcueille          #+#    #+#             */
-/*   Updated: 2021/06/20 19:56:09 by jcueille         ###   ########.fr       */
+/*   Updated: 2021/08/12 11:34:38 by atrouill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	is_valid_arg(t_list **tmp)
+{
+	size_t	i;
+	int		flag;
+
+	flag = 0;
+	while ((*tmp))
+	{
+		i = 1;
+		if ((*tmp)->content[0] == '-')
+		{
+			while ((*tmp)->content[i] == 'n')
+				i++;
+			if ((*tmp)->content[i] == '\0')
+				flag = 1;
+			else
+				return (flag);
+			(*tmp) = (*tmp)->next;
+		}
+		else
+			return (flag);
+	}
+	return (flag);
+}
 
 int	ft_echo(t_list *cmd)
 {
@@ -18,12 +43,7 @@ int	ft_echo(t_list *cmd)
 	int		flag;
 
 	tmp = cmd->next;
-	flag = 0;
-	if (tmp && !(ft_strcmp(tmp->content, "-n")))
-	{
-		flag = 1;
-		tmp = tmp->next;
-	}
+	flag = is_valid_arg(&tmp);
 	if (!(tmp))
 		return (0);
 	while (tmp)
