50f943baa214e3da7d80567fc90437aad22d9332
Arthur-TRT <atrouill@student.42.fr>
2021-11-19 15:55:15 +0100

    Bad check order corrected

--- srcs/builtins/ft_exit.c
+++ srcs/builtins/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: atrouill <atrouill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/21 09:51:26 by atrouill          #+#    #+#             */
-/*   Updated: 2021/11/17 16:58:03 by atrouill         ###   ########.fr       */
+/*   Updated: 2021/11/19 14:19:33 by atrouill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,7 @@ int	ft_exit(t_list *cmd, t_list **old_cmds, char **splitted, t_exec *exec)
 {
 	int	exit_code;
 
-	if (cmd->next && cmd->next->next)
-	{
-		ft_putendl_fd("minishell: exit: too many arguments", 2);
-		return (1);
-	}
-	else if (cmd->next == NULL)
+	if (cmd->next == NULL)
 		exit_code = 0;
 	else if (ft_strisdigit(cmd->next->content) == false)
 	{
@@ -45,6 +40,11 @@ int	ft_exit(t_list *cmd, t_list **old_cmds, char **splitted, t_exec *exec)
 		ft_putstr_fd(cmd->next->content, 2);
 		ft_putendl_fd(": numeric argument required", 2);
 		exit_code = 2;
+	}
+	else if (cmd->next && cmd->next->next)
+	{
+		ft_putendl_fd("minishell: exit: too many arguments", 2);
+		return (1);
 	}
 	else
 		exit_code = ft_atoi(cmd->next->content);
